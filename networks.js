eec37e43a8ffd4e4b6a72fd28c2b23fe3b4e0ca8
pesetachain <admin@pesetachain.info>
2014-12-13 19:27:57 +0100

    Update networks.js

--- networks.js
+++ networks.js
@@ -32,21 +32,21 @@ exports.mainnet = exports.livenet;
 
 exports.testnet = {
   name: 'testnet',
-  magic: hex('0b110907'),
+  magic: hex('fcc1b7dc'),
   addressVersion: 0x6f,
   privKeyVersion: 239,
   P2SHVersion: 196,
   hkeyPublicVersion: 0x043587cf,
   hkeyPrivateVersion: 0x04358394,
   genesisBlock: {
-    hash: hex('43497FD7F826957108F4A30FD9CEC3AEBA79972084E90EAD01EA330900000000'),
+    hash: hex('3864908c81344692075f051b011c762e195ccdb1003673ffbf5132b53058feed'),
     merkle_root: hex('3BA3EDFD7A7B12B27AC72C3E67768F617FC81BC3888A51323A9FB8AA4B1E5E4A'),
     height: 0,
-    nonce: 414098458,
+    nonce: 1358256,
     version: 1,
     prev_hash: buffertools.fill(new Buffer(32), 0),
-    timestamp: 1296688602,
-    bits: 486604799,
+    timestamp: 1388512432,
+    bits: 504365040,
   },
   dnsSeeds: [
   ],
